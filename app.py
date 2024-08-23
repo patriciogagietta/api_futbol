edd8550834e99eecc8175087881cf1453800b523
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-08-23 16:55:33 -0300

    arreglo de error que no creaba las tablas

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Crear las tablas si no existen
     app.run(host='localhost', port=5000, debug=True)
